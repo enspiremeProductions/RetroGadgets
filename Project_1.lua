a452f8b3e9d1be893c571675aab61402422aff20
enspiremeProductions <120911228+enspiremeProductions@users.noreply.github.com>
2022-12-18 02:42:53 -0800

    Update Project_1.lua

--- Project_1.lua
+++ Project_1.lua
@@ -14,7 +14,7 @@ local segDisp:SegmentDisplay = gdt.SegmentDisplay0
 --             Common Anode
 --	     --	  	 1
 --      |  |  	6 2
---  	   --      7
+--  	 --      7
 --      |  |    5 3
 --    	 -- .    4 8
 local segCode:{{number}} = {
@@ -64,59 +64,56 @@ function bcdSplit(display:SegmentDisplay, value:number)
     displayDigit(display, 1, digit1) --MSDigit
 end
 
-
 --Displays count on LED strips
 function displayLEDs(value:number)
 	-- leftmost led strip case
     if value <= 8 then 
         for i = 1,8 do
-        		led0.States[i] = false
-						led1.States[1] = false
-				end
-				for i = 1, value do
-            led0.States[i] = true
-						
+    		led0.States[i] = false
+			led1.States[1] = false
+		end
+		for i = 1, value do
+            led0.States[i] = true						
         end
     end		
 	-- middle led strip case
-		if value > 8 and value <= 16 then
-				for i = 1,8 do
-        		led1.States[i] = false
-						led2.States[1] = false
-			  end
-				for i = 1, value - 8 do
-						led1.States[i] = true
-				end													
-		end
+	if value > 8 and value <= 16 then
+		for i = 1,8 do
+			led1.States[i] = false
+			led2.States[1] = false
+	    end
+		for i = 1, value - 8 do
+			led1.States[i] = true
+		end													
+	end
 	-- rightmost led strip case
-		if value > 16 then
-				for i = 1,8 do
-        		led2.States[i] = false
-							
-    		end
-				for i = 1, value - 16 do
-						led2.States[i] = true
-				end		
-		end					
+	if value > 16 then
+		for i = 1,8 do
+			led2.States[i] = false				
+	end
+		for i = 1, value - 16 do
+			led2.States[i] = true
+		end		
+	end					
 end
 
 function updateColor()
-		if resetBtn.ButtonDown then
-				for i = 1,8 do
-							led0.Colors[i] = Color(Rsel.Value,Gsel.Value,Bsel.Value)																					led1.Colors[i] = Color(Rsel.Value,Gsel.Value,Bsel.Value)																					led2.Colors[i] = Color(Rsel.Value,Gsel.Value,Bsel.Value)
-							for k = 1,2 do
-									segDisp.Colors[k][i] = Color(Rsel.Value,Gsel.Value,Bsel.Value)
-							end
-				end
+	if resetBtn.ButtonDown then
+		for i = 1,8 do
+			led0.Colors[i] = Color(Rsel.Value,Gsel.Value,Bsel.Value)																					led1.Colors[i] = Color(Rsel.Value,Gsel.Value,Bsel.Value)																					led2.Colors[i] = Color(Rsel.Value,Gsel.Value,Bsel.Value)
+			for k = 1,2 do
+				segDisp.Colors[k][i] = Color(Rsel.Value,Gsel.Value,Bsel.Value)
+			end
 		end
+	end
 end
 
 --Edge Detects btn pressed and updates counter :: single event
 function updateCounter()
-		if count >= 0 or count <= maxLEDs then
-        if upBtn.ButtonDown then count += 1 end
-	    	if downBtn.ButtonDown then count -= 1 end
-    end
+	if count >= 0 or count <= maxLEDs then
+    	if upBtn.ButtonDown then count += 1 end
+		if downBtn.ButtonDown then count -= 1 end
+	end
     if count < 0 then count = 0 end     --counter lower limit
     if count > maxLEDs then count = maxLEDs end   --counter upper limit
   --Function calls with new count  
@@ -125,6 +122,6 @@ function updateCounter()
 end
 
 function update()
-  updateCounter() --updates every tick :: clk freq TBD still
+	updateCounter() --updates every tick :: clk freq TBD still
 	updateColor()
 end
